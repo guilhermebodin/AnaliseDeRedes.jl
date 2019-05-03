93c73aad781bbc8e0dc6e26a54bf608c2cbc0158
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-05-03 10:56:12 -0300

    cc funcionando

--- src/shortcircuit.jl
+++ src/shortcircuit.jl
@@ -7,16 +7,18 @@ function shortcircuit(impedancias, bus, idxbuses, fase, Vth)
         Zth[i] = smartZth(Ybus, bus)
         i+=1
     end
+    Iflt = 1/sum(Zth)
     if fase == "a"
-        Iflt = 1/(SEQ2PHASE[1, :]'*Zth)
+        Iseq = PHASE2SEQ*[Iflt; 0; 0]
+        Vseq = [-Zth[3]*Iflt; Vth - Zth[1]*Iflt; -Zth[2]*Iflt]
     elseif fase == "b"
-        Iflt = 1/(SEQ2PHASE[2, :]'*Zth)
+        Iseq = PHASE2SEQ*[0; Iflt; 0]
+        Vseq = [-Zth[3]*Iflt; ANG*(Vth - Zth[1]*Iflt); ANG^2*-Zth[2]*Iflt]
     elseif fase == "c"
-        Iflt = 1/(SEQ2PHASE[3, :]'*Zth)
+        Iseq = PHASE2SEQ*[0; 0; Iflt]
+        Vseq = [-Zth[3]*Iflt; ANG^2*(Vth - Zth[1]*Iflt); ANG*-Zth[2]*Iflt]
     else
         error("Fase só pode ser a, b ou c")
     end
-    Iseq = [Iflt; Iflt; Iflt]
-    Vseq = [-Zth[3]*Iflt; Vth - Zth[1]*Iflt; -Zth[2]*Iflt]
     return SEQ2PHASE*Iseq, SEQ2PHASE*Vseq
 end
